1b0f8bdf7b66d982422aa15ca3bfb88a89bac70b
Alberto Pochettino <pochettino.alberto@protonmail.com>
2025-11-20 12:59:52 +0100

    START conf sonarqube

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -1,8 +1,14 @@
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // run both unit and integration tests
+
   roots: ['<rootDir>/test_unit', '<rootDir>/test_integration'],
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+
+  // --- Add these ---
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text', 'html'],
 };
