a227cadb8d23593e4c3d3c4532c00a810146bf35
sebnem <sebnmuslu@gmail.com>
2026-01-03 21:48:43 +0100

    fix: sonarcloud tests

--- back-end/test_e2e/globalTeardown.ts
+++ back-end/test_e2e/globalTeardown.ts
@@ -1,5 +1,7 @@
 import { teardownTestDb } from './testDb'
+import db from '../src/dao/db'
 
 export default async function globalTeardown() {
     await teardownTestDb()
+    db.close()
 }
--- back-end/test_e2e/setup.ts
+++ back-end/test_e2e/setup.ts
@@ -1,8 +1,3 @@
-/// <reference types="jest" />
-process.env.NODE_ENV = 'test'
-process.env.SUPABASE_URL = 'http://test.supabase.co';
-process.env.SUPABASE_SERVICE_KEY = 'test-key';
-
 import request from 'supertest'
 import { dbReady } from '../src/dao/db'
 import { app } from '../index'
@@ -10,6 +5,6 @@ import { app } from '../index'
 beforeAll(async () => {
     // Ensure the database is fully initialized before any test runs
     await dbReady
-}, 60000)
+})
 
 export default request(app)
