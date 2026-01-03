d82a1cc996537d85c5c0be166cfbf81797664d0f
sebnem <sebnmuslu@gmail.com>
2026-01-04 00:00:34 +0100

    fix: sonarcloud tests

--- back-end/test_e2e/setup.ts
+++ back-end/test_e2e/setup.ts
@@ -1,10 +1,24 @@
-import { app } from '../index'
-export defaimport request from 'supertest'
-import { app } from '../index'
+/// <reference types="jest" />
+// Ensure NODE_ENV=test to let db.ts pick the test DB path
+process.env.NODE_ENV = 'test'
+
+import request from 'supertest'
+// Remove resetTestDb call - now handled globally
+// Remove teardownTestDb import and call - now handled globally
+
+// Import dbReady after NODE_ENV has been set
 import { dbReady } from '../src/dao/db'
 
+// import app after NODE_ENV has been set
+import { app } from '../index'
+
+// jest global setup: nothing to export; tests will import this file.
+
+// Remove afterAll hook - database teardown now handled globally
+
+// Ensure DB initialization finished before any tests run
 beforeAll(async () => {
     await dbReady
-})
+}, 60000) // 60 second timeout for DB initialization
 
-export default request(app)ult app
+export default request(app)
