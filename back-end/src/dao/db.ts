1a5c6649b065fd12581db20589ee38edfbed09c4
sebnem <sebnmuslu@gmail.com>
2026-01-04 19:32:59 +0100

    fix: sonarcloud tests

--- back-end/src/dao/db.ts
+++ back-end/src/dao/db.ts
@@ -133,14 +133,30 @@ function initializeDb(dbInstance: any) {
     }
 }
 
-/* ✅ THIS IS THE MISSING PIECE */
 export const closeDb = (): Promise<void> => {
     return new Promise((resolve, reject) => {
-        db.close(err => {
-            if (err) reject(err)
-            else resolve()
-        })
+        try {
+            // 🔑 REQUIRED for WAL mode
+            db.exec("PRAGMA wal_checkpoint(TRUNCATE);", err => {
+                if (err) {
+                    // even if checkpoint fails, still try to close
+                    db.close(closeErr => {
+                        if (closeErr) reject(closeErr)
+                        else resolve()
+                    })
+                } else {
+                    db.close(closeErr => {
+                        if (closeErr) reject(closeErr)
+                        else resolve()
+                    })
+                }
+            })
+        } catch (e) {
+            // last-resort close
+            db.close(() => resolve())
+        }
     })
 }
+
 
 export default db
